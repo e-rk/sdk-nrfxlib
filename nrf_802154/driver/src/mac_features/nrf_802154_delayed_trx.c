8615e7390fab4bc48b4b4320a3a08b87acf0847f
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2024-11-05 21:30:10 +0100

    802154

--- nrf_802154/driver/src/mac_features/nrf_802154_delayed_trx.c
+++ nrf_802154/driver/src/mac_features/nrf_802154_delayed_trx.c
@@ -75,8 +75,8 @@
 #define TX_SETUP_TIME_MAX 360u ///< Maximum time needed to prepare TX procedure [us]. It does not include TX ramp-up time.
 #define RX_SETUP_TIME_MAX 290u ///< Maximum time needed to prepare RX procedure [us]. It does not include RX ramp-up time.
 #elif defined(NRF54L_SERIES)
-#define TX_SETUP_TIME_MAX 600u ///< Maximum time needed to prepare TX procedure [us]. It does not include TX ramp-up time.
-#define RX_SETUP_TIME_MAX 600u ///< Maximum time needed to prepare RX procedure [us]. It does not include RX ramp-up time.
+#define TX_SETUP_TIME_MAX 400u ///< Maximum time needed to prepare TX procedure [us]. It does not include TX ramp-up time.
+#define RX_SETUP_TIME_MAX 400u ///< Maximum time needed to prepare RX procedure [us]. It does not include RX ramp-up time.
 #elif defined(NRF54H_SERIES)
 #ifndef TX_SETUP_TIME_MAX
 #define TX_SETUP_TIME_MAX 400u ///< Maximum time needed to prepare TX procedure [us]. It does not include TX ramp-up time.
